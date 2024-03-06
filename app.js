5b2750eb99549f310386837f22932244d251c0ec
binhdv37 <vanbinhdoan971@gmail.com>
2024-03-06 08:34:29 +0700

    chore: update cache-control
    Signed-off-by: binhdv37 <vanbinhdoan971@gmail.com>

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'), {maxAge: '1h', etag: false}));
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: 3_600_000}));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
